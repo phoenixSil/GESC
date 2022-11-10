5dffc5986abd31134cb92ea660d622ace5865996
Sillery TALLA Philinte <tsillery@ymail.com>
2022-11-09 22:35:07 -0500

    Ajout Des Services

---
+++ Gesc.Api/Services/Contrats/IServiceDeFiliere.cs
@@ -0,0 +1,14 @@
+﻿using Gesc.Api.Dtos.Config.Filieres;
+using MsCommun.Reponses;
+
+namespace Gesc.Api.Services.Contrats
+{
+    public interface IServiceDeFiliere
+    {
+        public Task<List<FiliereDto>> LireToutesLesFilieres();
+        public Task<ReponseDeRequette> AjouterUneFiliere(FiliereACreerDto filiereAAjouter);
+        public Task<ReponseDeRequette> SupprimerUneFiliere(Guid FiliereId);
+        public Task<FiliereDetailDto> LireDetailDuneFiliere(Guid id);
+        public Task<ReponseDeRequette> ModifierUneFiliere(Guid filiereId, FiliereAModifierDto filiereAModifierDto);
+    }
+}
