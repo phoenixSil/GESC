3c5d91ac1c07763e6d816ea05a53c6e500160941
Sillery TALLA Philinte <tsillery@ymail.com>
2022-11-09 22:43:00 -0500

    Mise a Jour du fichier de demmarage program.cs

--- Gesc.Api/Program.cs
+++ Gesc.Api/Program.cs
@@ -1,4 +1,40 @@
+using MsCommun.Extensions;
+using Gesc.Api.Extensions;
+using Gesc.Api.GeneralExtensions;
+using Gesc.Api.Datas;
+using Serilog;
+using MsCommun.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuration Serilog
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+
+Log.Information("GSCE Demmarre demarre ");
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+//Add services to the container.
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddSqlServerDbConfiguration<SchoolConfigDbContext>(builder.Configuration.GetConnectionString("appConnString"));
+}
+else
+{
+    builder.Services.AddInMemoryDataBaseConfiguration<SchoolConfigDbContext>("InMem");
+}
+
+builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigureControllerServices();
+builder.Services.ConfigurePersistenceServices(builder.Configuration);
+builder.Services.AjoutterCoucheDesProxies(builder.Configuration);
+
 
 // Add services to the container.
 
