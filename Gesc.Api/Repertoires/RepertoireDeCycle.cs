5b3b3e770d491f50fd4e7327d42d1f2d5d3cb5a8
Sillery TALLA Philinte <tsillery@ymail.com>
2022-11-09 22:44:57 -0500

    Ajout de la Couche de Repository => Repertoires

---
+++ Gesc.Api/Repertoires/RepertoireDeCycle.cs
@@ -0,0 +1,13 @@
+﻿using Gesc.Api.Modeles.Config;
+using Gesc.Api.Repertoires.Contrats;
+using Gesc.Api.Datas;
+using MsCommun.Repertoires;
+
+namespace Gesc.Api.Repertoires
+{
+    public class RepertoireDeCycle : RepertoireGenerique<Cycle>, IRepertoireDeCycle
+    {
+        public RepertoireDeCycle(SchoolConfigDbContext context) : base(context)
+        { }
+    }
+}
---
+++ Gesc.Api/Repertoires/RepertoireDeFiliere.cs
@@ -0,0 +1,13 @@
+﻿using Gesc.Api.Modeles.Config;
+using Gesc.Api.Repertoires.Contrats;
+using Gesc.Api.Datas;
+using MsCommun.Repertoires;
+
+namespace Gesc.Api.Repertoires
+{
+    public class RepertoireDeFiliere : RepertoireGenerique<Filiere>, IRepertoireDeFiliere
+    {
+        public RepertoireDeFiliere(SchoolConfigDbContext context) : base(context)
+        { }
+    }
+}
